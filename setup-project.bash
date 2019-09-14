6ada80535a861e47f8c1b17dafbfc3d805c45df0
Gaël Lhez <glhez@users.noreply.github.com>
2019-09-14 15:11:46 +0200

    update git/editor config, update wrapper to mvn 3.6.2 and added setup-project.bash to copy common settings onto other repositories

---
+++ setup-project.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare SCRIPT_FILE="$(realpath "$0")"
+declare SCRIPT_DIR="${SCRIPT_FILE%/*}"
+
+if [[ -z "$TARGET_DIR" ]]; then
+  declare TARGET_DIR="$PWD"
+fi
+
+declare TARGET_DIR_REALPATH="$(realpath "${TARGET_DIR}")"
+
+if [[ -z "${TARGET_DIR_REALPATH}" || ! -d "${TARGET_DIR_REALPATH}" ]]; then
+  echo "error: invalid target directory. Try TARGET_DIR=/some/path $0"
+  exit 1
+fi
+
+if [[ "$SCRIPT_DIR" == "${TARGET_DIR_REALPATH}" ]]; then
+  echo "error: invalid target directory '${TARGET_DIR_REALPATH}'. Same as source '${SCRIPT_DIR}'"
+  exit 1
+fi
+
+TARGET_DIR="${TARGET_DIR_REALPATH}"
+
+echo ":: copying configuration files..."
+cp -v "${SCRIPT_DIR}/.editorconfig" "${SCRIPT_DIR}/.gitattributes" "${SCRIPT_DIR}/.gitignore" "${TARGET_DIR}"
+
+echo ":: copying maven wrapper files..."
+cp -v "${SCRIPT_DIR}/mvnw" "${SCRIPT_DIR}/mvnw.cmd" "${TARGET_DIR}"
+mkdir -pv "${TARGET_DIR}/.mvn/wrapper"
+cp "${SCRIPT_DIR}/.mvn/wrapper/maven-wrapper.properties" "${TARGET_DIR}/.mvn/wrapper"
+
+echo ":: fixing line ending"
+dos2unix "${SCRIPT_DIR}/mvnw"
+
+echo ":: done"
