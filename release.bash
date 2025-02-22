5deb83c4cd39d29894650da23082445dfdcefae6
Gaël LHEZ <glhez@users.noreply.github.com>
2025-02-22 12:58:23 +0100

    friendly ci, fix sortPom and editorconfig

--- release.bash
+++ release.bash
@@ -1,7 +1,48 @@
 #!/bin/bash
 
+usage() {
+  echo "
+NAME
+
+  ${0##*/} - release version using flatten maven plugin and friendly ci
+
+OPTIONS
+
+  -r, --release VERSION
+
+    Target release version (default to revision minus SNAPSHOT)
+
+  -d, --development VERSION
+
+    Target development version (default to releaseVersion + 1 + SNAPSHOT)
+
+"
+}
+
 main() {
-          return 0
+  if ! TEMP=$(getopt -o 'hr:d:' --long 'help,release:,development:' -n "${0##*/}" -- "$@") ; then
+    echo 'Terminating...' >&2
+    return 1
+  fi
+
+  eval set -- "$TEMP"
+  unset TEMP
+
+
+  local releaseVersion=''
+  local developmentVersion=''
+  while true; do
+    case "$1" in
+      -h|--help)        usage ; return 2 ;;
+      -r|--release)     releaseVersion="$2"     ; shift 2 ; continue ;;
+      -d|--development) developmentVersion="$2" ; shift 2 ; continue ;;
+      --)                                         shift   ; break ;;
+      *)  echo "internal error" >&2 ; return 1 ;;
+    esac
+  done
+  for arg; do
+    echo "$arg"
+  done
 }
 
 main "$@"
